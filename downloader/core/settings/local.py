d51e7f9da804b447c48afce2267d2636dbf8df84
StanMathers <sabagamgebeli@gmail.com>
2023-11-28 21:14:31 +0400

    Added local configuration and reconfigured manage.py

--- downloader/core/settings/local.py
+++ downloader/core/settings/local.py
@@ -1,5 +1,27 @@
-from base import *
+import os
+from .base import *
 
+import environ
 
+env_file = os.path.join(BASE_DIR.parent.parent, "dev.env")
 
+environ.Env.read_env(env_file)
+env = environ.Env()
 
+# DATABASE
+POSTGRES_DB = env("POSTGRES_DB")
+POSTGRES_USER = env("POSTGRES_USER")
+POSTGRES_PASSWORD = env("POSTGRES_PASSWORD")
+POSTGRES_HOST = env("POSTGRES_HOST")
+POSTGRES_PORT = env("POSTGRES_PORT")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": POSTGRES_PORT,
+    }
+}
