4a6e51461e1c354de0347026962a66a5c829fe36
StanMathers <sabagamgebeli@gmail.com>
2023-11-29 14:52:31 +0400

    Prepared for render deployment

--- downloader/core/settings/prod.py
+++ downloader/core/settings/prod.py
@@ -1,23 +1,17 @@
 import os
 from .base import *
+import dj_database_url
 
 import environ
 
-environ.Env.read_env()
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR.parent.parent, ".env"))
+
 
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+EXTERNAL_DATABASE_URL = env("EXTERNAL_DATABASE_URL")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": POSTGRES_DB,
-        "USER": POSTGRES_USER,
-        "PASSWORD": POSTGRES_PASSWORD,
-        "HOST": POSTGRES_HOST,
-        "PORT": POSTGRES_PORT,
-    }
-}
+DATABASES = {"default": dj_database_url.parse(EXTERNAL_DATABASE_URL)}
